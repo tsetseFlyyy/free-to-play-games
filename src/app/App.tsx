d3ea0fc16c855a8acd1cdef955158e1aff3ab72e
Shalkar <shalkar.koshenayev@gmail.com>
2025-02-07 19:26:05 +0500

    tanstack-query init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isPending, error, data } = useQuery({
+    queryKey: ["games"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://free-to-play-games-database.p.rapidapi.com/api/games",
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "x-rapidapi-key": import.meta.env.VITE_RAPIDAPI_KEY,
+            "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Ошибка при загрузке данных");
+      }
+
+      return response.json();
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {data &&
+        !isPending &&
+        data.map((item) => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+          </div>
+        ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
