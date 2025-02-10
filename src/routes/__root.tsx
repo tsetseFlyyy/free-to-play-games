f64f7e97ebd1ea4942dca8a558560e00bd3e6005
Shalkar <shalkar.koshenayev@gmail.com>
2025-02-10 11:32:46 +0500

    delete Header component, clear app.css

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
-import Header from "@/widgets/header";
 import { Toaster } from "@/shared/ui/toaster";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,7 +9,25 @@ function RootComponent() {
   return (
     <>
       <div className="sticky z-10 w-full bg-white border-b">
-        <Header />
+        <header className="container mx-auto flex items-center h-16">
+          <Link
+            to="/"
+            activeOptions={{ exact: true }}
+            activeProps={{ className: "text-black" }}
+          >
+            <h1 className="text-2xl font-semibold">Free-To-Play Games</h1>
+          </Link>
+          <div className="ml-16">
+            <Link
+              to="/favorites"
+              activeProps={{ className: "text-black underline" }}
+            >
+              <span className="text-base transition hover:text-gray-500">
+                Favorites
+              </span>
+            </Link>
+          </div>
+        </header>
       </div>
       <main>
         <Outlet />
